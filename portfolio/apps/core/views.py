52b3d71c048390d9e2ed7dda3fd9d758b8dd5b36
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-01-14 23:25:14 -0800

    host, not path...

--- portfolio/apps/core/views.py
+++ portfolio/apps/core/views.py
@@ -19,7 +19,7 @@ class IndexView(ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
 
-        context["staging"] = True if "staging" in self.request.path else False
+        context["staging"] = True if "staging" in self.request.get_host() else False
 
         return context
 
